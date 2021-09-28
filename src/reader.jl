0fce7e1ce8dddf4c13ca7a362a8e4984e28e836e
Neel Smith <dnsmith.neel@gmail.com>
2021-09-28 16:13:48 -0400

    Start work on reading CEX

--- src/reader.jl
+++ src/reader.jl
@@ -11,4 +11,37 @@ $(SIGNATURES)
 """
 function catalog(cexsrc, delim = "|")
     allblocks = blocks(cexsrc)
+    collblocks = blocksfortype("citecollections", allblocks)
+    if isempty(collblocks)
+        throw(DomainError(cexsrc, "No citecollections block found in CEX source"))
+    end
+    collectionsdata = []
+    for cblock in collblocks
+        push!(collectionsdata, cblock.lines[2:end])
+    end
+    flatcollections = collectionsdata |> Iterators.flatten |> collect
+
+    propblocks = blocksfortype("citeproperties", allblocks)
+    if isempty(propblocks)
+        throw(DomainError(cexsrc, "No citeproperties block found in CEX source"))
+    end
+    propertiesdata = []
+    for pblock in propblocks
+        push!(propertiesdata, pblock.lines[2:end])
+    end
+    flatproperties = propertiesdata |> Iterators.flatten |> collect
+
+    catalogurns = []
+    for coll in flatcollections
+        parts = split(coll, delim)
+        push!(catalogurns, Cite2Urn(parts[1]))
+    end
+    catalogurns
+    propertiesurns = []
+    for prop in flatproperties
+        parts = split(prop, delim)
+        push!(propertiesurns, Cite2Urn(parts[1]) |> dropobject)
+    end
+    propertiesurns |> unique
+
 end
