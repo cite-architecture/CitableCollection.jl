7c50cddedc773799316c819e5c3382ca143ae6e4
Neel Smith <dnsmith.neel@gmail.com>
2022-01-24 18:27:13 -0500

    Add breaking redefinition of `CatalogedCollection` structure

---
+++ test/test_catalog.jl
@@ -0,0 +1,7 @@
+@testset "Test CITE properties of a catalog" begin
+    #
+    # citablecollectiontrait
+    # urn comparison
+    # cex serialization
+    # 5 table functions
+end
---
+++ test/test_catentry.jl
@@ -0,0 +1,5 @@
+#=
+citable
+urncomparison
+cex
+=#
