0b99cfe9387913c8e30e15900fabe264d9fbdade
Neel Smith <dnsmith.neel@gmail.com>
2022-02-04 09:42:26 -0500

    Better errorchecking

--- src/collection_data_cooked.jl
+++ src/collection_data_cooked.jl
@@ -9,7 +9,7 @@ function strictread(cexsrc::AbstractString, delimiter = "|")
     propslist = propertiesfromcex(cexsrc, delimiter)
     colnamescheck = columnnamesok(rdclist, propslist)
     if colnamescheck == false
-        DomainError(cexsrc, "Invalid configuration of CITE collection.")
+        DomainError(propslist, "Invalid configuration of CITE collection.")
     else
         converted = RawDataCollection[]
         # Now convert types as needed.
@@ -38,21 +38,40 @@ function converttypes(rdc::RawDataCollection, rdcprops::Vector{PropertyDefinitio
         colname = sch.names[colidx]
         coltype = rdcprop.property_type
         @warn("==>CITE type: $(sch.names[colidx]) $(coltype) $(propertyid(rdcprop.property_urn))")
-        if coltype == Cite2Urn && sch.types[colidx] != Missing
+        if coltype == Cite2Urn && ! (sch.types[colidx] <: AbstractString)
             @debug("SEE if  already converted: $(sch.types[colidx]) for $(sch.names[colidx])")
             if sch.types[colidx] == Cite2Urn 
                 @debug("ALREADY CONVERTED")
                 row = map(getproperty(colname), rdc.data)
                 push!(coldata, row)
             else
-                @debug("NOT yet converted. Creating Cite2Urns on column", colname)
-                urnrow = map(row -> Cite2Urn(row[colname]), rdc.data)
+                @warn("NOT yet converted. Creating Cite2Urns on column/type", colname, sch.types[colidx])
+
+
+                #map(row -> Cite2Urn(row[colname]), rdc.data)
+                urnrow = []
+                for r in rdc.data
+                    if ismissing(r[colname])
+                        push!(urnrow, missing)
+                    else
+                        push!(urnrow, Cite2Urn(strip(r[colname])))
+                    end
+                end
                 push!(coldata, urnrow)
             end
          
             
-        elseif coltype == CtsUrn && sch.types[colidx] != Missing
-            urnrow = map(row -> CtsUrn(row[colname]), rdc.data)
+        elseif coltype == CtsUrn && ! (sch.types[colidx] <: AbstractString)
+            #urnrow = map(row -> CtsUrn(row[colname]), rdc.data)
+            #map(row -> Cite2Urn(row[colname]), rdc.data)
+            urnrow = []
+            for r in rdc.data
+                if ismissing(r[colname])
+                    push!(urnrow, missing)
+                else
+                    push!(urnrow, CtsUrn(strip(r[colname])))
+                end
+            end
             push!(coldata, urnrow)
         else
             @debug("REUSE column as is.")
@@ -74,7 +93,7 @@ function columnnamesok(rdclist::Vector{RawDataCollection}, propertieslist::Vecto
         set2 = Tables.columnnames(rdc.data) .|> string .|> lowercase |> Set
         @debug(">Compare ", set1, set2)
         if set1 != set2
-            @warn("Column names in table did not match configured values for collection $(urn(rdc)): $(set1) != $(set2)")
+            @warn("Column names in table did not match configured values for collection $(urn(rdc)): table/configuration:",set1, set2)
             return false
         end
     end
